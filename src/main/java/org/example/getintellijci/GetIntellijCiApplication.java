d4c658abf0c1adfabe809bec2dc72795dec1c5d9
selma <manounisalma@gmail.com>
2025-02-20 11:55:53 +0100

    initial commit

---
+++ src/main/java/org/example/getintellijci/GetIntellijCiApplication.java
@@ -0,0 +1,13 @@
+package org.example.getintellijci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetIntellijCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetIntellijCiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/getintellijci/GetIntellijCiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.getintellijci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetIntellijCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
